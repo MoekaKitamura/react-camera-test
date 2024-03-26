1d73ee58efcc21d8ee0eac3a1b9d9b9279f29723
MoekaKitamura <moeka.k76@gmail.com>
2024-03-26 00:31:08 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React, { useRef } from "react";
+import "./App.css";
+
+const App = () => {
+  const imgRef = useRef(null);
+  const fileRef = useRef(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const file = fileRef.current.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      fetch("api/Upload", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(btoa(reader.result)),
+      });
+    };
+    reader.readAsBinaryString(file);
+  };
+
+  const handleChange = () => {
+    const file = fileRef.current.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      imgRef.current.src = reader.result;
+    };
+    reader.readAsDataURL(file);
+  };
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <hr />
+      <input
+        type="file"
+        accept="image/*;capture=camera"
+        onChange={handleChange}
+        ref={fileRef}
+      />
+      <hr />
+      <img ref={imgRef} width={200} />
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default App;
